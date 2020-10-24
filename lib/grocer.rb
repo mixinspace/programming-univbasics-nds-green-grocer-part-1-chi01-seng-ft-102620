137ab50021ade3772fbfe53550b09e5396504c94
Islam Madkaur <islammadkaur@gmail.com>
2020-10-24 22:40:58 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,8 +2,8 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   index = 0
-  collection.each do |grocery_item|
-    return grocery_item if grocery_item[:item] === name
+  collection.each do |items|
+    return items if items[:item] === name
       index += 1
     end
 end
